c55d71bd3d6ea288b5cd3ef1661e741205415dca
Sayed Aman <sayedamancoder@gmail.com>
2026-06-03 20:22:20 +0530

    updtaed transporter code

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,6 +6,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: process.env.BREVO_SMTP_HOST,
   port: process.env.BREVO_SMTP_PORT,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.BREVO_SMTP_USER,
     pass: process.env.BREVO_SMTP_PASS,
